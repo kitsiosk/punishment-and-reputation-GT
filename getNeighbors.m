7eb44acf091b062bf84d03d39b74ca2c2834322d
Thanos <thanosp2@yahoo.gr>
2020-05-15 23:36:51 +0300

    Fixed plotHex and getNrighbors to match the new lattice

--- getNeighbors.m
+++ getNeighbors.m
@@ -1,20 +1,20 @@
 function R = getNeighbors(i,j,M,N)
 
 % Find neighbors for general case
-if mod(i,2) == 0
-    R = [i+1, j;
+if mod(j,2) == 0
+    R = [i,   j+1;
          i+1, j+1;
-         i,   j+1;
-         i-1, j+1;
-         i-1, j;
-         i,   j-1];
-else
-    R = [i+1, j-1;
          i+1, j;
+         i+1, j-1;
+         i,   j-1;
+         i-1, j];
+else
+    R = [i-1, j+1;
          i,   j+1;
-         i-1, j;
+         i+1, j;
+         i,   j-1;
          i-1, j-1;
-         i,   j-1];
+         i-1, j];
 end
 
 % Exclude corner cases
@@ -23,6 +23,6 @@ R(R(:,1) == M+1,:) = [];
 R(R(:,2) == N+1,:) = [];
 
 % Convert to column major indexing
-R =  R(:,1) + (R(:,2) - 1) * N;
+R =  R(:,1) + (R(:,2) - 1) * M;
 
 end
