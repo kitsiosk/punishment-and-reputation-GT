64fb22c8e91e5efc9ec2562148fbf1eeb2e5e75f
Thanos <thanosp2@yahoo.gr>
2020-05-15 22:03:56 +0300

    Minor improvements + new meet names

--- main.m
+++ main.m
@@ -2,15 +2,15 @@
 clear
 close all
 
-% Type of game: 'simple', 'punishment', 'reputation'
-mode = 'punishment';
+% Type of game: 'Simple', 'Punishment', 'Reputation'
+mode = 'Simple';
 
 % Dimentions of lattice
 M = 100;
 N = 100;
 
 % Number of rounds
-T = 2000;
+T = 500;
 
 % Investment multiplication factor
 r = 2;
@@ -27,24 +27,44 @@ beta = 1.5;
 % 2 for defectors with punishment( 'punishment' mode only )
 % 3 for cooperators with punishment( 'punishment' mode only)
 % rng(1)
-% L = unidrnd(2,M,N) - 1;
-L = randi([0 3], M, N);
-fprintf('Initial percentage of cooperators: %0.2f %% \n', ...
-    (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N))
-printPercentages(L)
+if strcmp(mode, 'Simple')
+    L = unidrnd(2,M,N) - 1;
+    % meet function handle
+    meet = @meetSimple;
+    
+elseif strcmp(mode, 'Punishment')
+    L = randi([0 3], M, N);
+    % meet function handle
+    meet = @meetPunishment;
+    fprintf('Initial percentage of cooperators: %0.2f %% \n', ...
+        (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N))
+    printPercentages(L)
+    
+elseif strcmp(mode, 'Reputation')
+    % meet function handle
+    meet = @meetReputation;
+    % TODO
+else
+    fprintf("Invalid mode entered.\n")
+    fprintf("Please enter 'Simple', 'Punishment' or 'Reputation'.\n")
+    return
+end
 
 % Score matrix initialization
 P = zeros(M,N);
-perc = []; % percentages array
+% Percentages array
+perc = [];
 
 for t = 1:T
-%% INTERACTION STAGE
+    %% INTERACTION STAGE
+    
     if mod(t, 10) == 0
         pp = (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N);
         fprintf('Percentage of cooperators at round %d: %f%% \n', t, pp)
         printPercentages(L);
         perc = [perc; pp];
     end
+    
     % For every point in the lattice
     for i = 1:M
         for j = 1:N
@@ -55,32 +75,23 @@ for t = 1:T
             
             % For every neighbor
             for k = 1:(lR-1)
-                if( strcmp(mode, 'simple') )
-                    P(i,j) = P(i,j) + meet(L(i,j), L(R(k)), L(R(k+1)), r, c);
-                elseif( strcmp(mode, 'punishment') )
-                    P(i,j) = P(i,j) + meetPunish(L(i,j), L(R(k)), L(R(k+1)), ...
-                        r, c, beta, gamma);
-                end     
+                P(i,j) = P(i,j) + meet(L(i,j), L(R(k)), L(R(k+1)), ...
+                    r, c, beta, gamma);
             end
             
             % If there are more than two neighbors,
             % then meet with the last and first neighbor
             % and find the mean score
             if lR > 2
-                if( strcmp(mode, 'simple') )
-                    P(i,j) = P(i,j) + meet(L(i,j), L(R(lR)), L(R(1)), r, c);
-                elseif( strcmp(mode, 'punishment') )
-                    P(i,j) = P(i,j) + meetPunish(L(i,j), L(R(lR)), L(R(1)), ...
-                        r, c, beta, gamma);
-                end
-%                 P(i,j) = P(i,j) + meet(L(i,j),L(R(lR)),L(R(1)),r,c);
+                P(i,j) = P(i,j) + meet(L(i,j), L(R(k)), L(R(k+1)), ...
+                    r, c, beta, gamma);
             end
             P(i,j) = P(i,j) / lR;
-
+            
         end
     end
     
-%% IMITATION/REPRODUCTION STAGE
+    %% IMITATION/REPRODUCTION STAGE
     
     % For every point in the lattice
     for i = 1:M
@@ -91,11 +102,6 @@ for t = 1:T
             
             % Difference of the scores compared to the neighbors
             diff = P(R) - P(i,j);
-            
-%             % Exclude neighbors with lower or equal scores from imitation
-%             negative_diff = diff < 1e-6;
-%             R(negative_diff) = [];
-%             diff(negative_diff) = [];
             
             imitationNeighboor = rouletteWheelSelection(diff);
             if imitationNeighboor ~= -1
@@ -111,6 +117,7 @@ end
 figure(1)
 plotHex(L)
 title('Final grid')
+
 figure(2)
 tm = (1:T/100)*100;
 plot(tm, perc);
--- rouletteWheelSelection.m
+++ rouletteWheelSelection.m
@@ -1,15 +1,11 @@
 function k = rouletteWheelSelection(fitness)
 % Choose an index with probability proportional to the fitness
 
-% Find cumulative sum of normalized fitness in [0,1]
-% fitness_cdf = cumsum(fitness/sum(fitness));
-sum_fitness = 0;
-for i=1:length(fitness)
-    if( fitness(i) > 0 )
-        sum_fitness = sum_fitness + fitness(i);
-    end
-end
-% Choose k with probability proportionate to diff
+% Find sum of positive fitness
+fitness(fitness < 1e-6) = 0;
+sum_fitness = sum(fitness);
+
+% Choose k with probability proportionate to fitness
 for k = 1:length(fitness)
     random_num = rand();
     if random_num <= fitness(k)/sum_fitness
