ee724d1c4d52ca84ab06f2cb6303a851ab55d7bc
Thanos <thanosp2@yahoo.gr>
2020-05-16 14:44:47 +0300

    main and getNeighboors optimizations

--- getNeighbors.m
+++ getNeighbors.m
@@ -1,26 +1,96 @@
 function R = getNeighbors(i,j,M,N)
 
 % Find neighbors for general case
-if mod(j,2) == 0
-    R = [i,   j+1;
-         i+1, j+1;
-         i+1, j;
-         i+1, j-1;
-         i,   j-1;
-         i-1, j];
+if i ~= 1 && i ~= M && j ~= 1 && j ~= N
+    if mod(j,2) == 0
+        R = [i,   j+1;
+             i+1, j+1;
+             i+1, j;
+             i+1, j-1;
+             i,   j-1;
+             i-1, j];
+    else
+        R = [i-1, j+1;
+             i,   j+1;
+             i+1, j;
+             i,   j-1;
+             i-1, j-1;
+             i-1, j];
+    end
+% Find corner cases
+elseif i == 1
+    if j == 1
+        R = [i,   j+1;
+             i+1, j];
+    elseif j == N
+        if mod(j,2) == 0
+            R = [i+1, j;
+                 i+1, j-1;
+                 i,   j-1];
+        else
+            R = [i+1, j;
+                 i,   j-1];
+        end
+    else
+        if mod(j,2) == 0
+            R = [i,   j+1;
+                 i+1, j+1;
+                 i+1, j;
+                 i+1, j-1;
+                 i,   j-1];
+        else
+            R = [i,   j+1;
+                 i+1, j;
+                 i,   j-1];
+        end
+    end
+elseif i == M
+    if j == 1
+        R = [i-1, j+1;
+             i,   j+1;
+             i-1, j];
+    elseif j == N
+        if mod(j,2) == 0
+            R = [i,   j-1;
+                 i-1, j];
+        else
+            R = [i,   j-1;
+                 i-1, j-1;
+                 i-1, j];
+        end
+    else
+        if mod(j,2) == 0
+            R = [i,   j+1;
+                 i,   j-1;
+                 i-1, j];
+        else
+            R = [i-1, j+1;
+                 i,   j+1;
+                 i,   j-1;
+                 i-1, j-1;
+                 i-1, j];
+        end
+    end
 else
-    R = [i-1, j+1;
-         i,   j+1;
-         i+1, j;
-         i,   j-1;
-         i-1, j-1;
-         i-1, j];
+    if j == 1
+        R = [i-1, j+1;
+             i,   j+1;
+             i+1, j;
+             i-1, j];
+    elseif j == N
+        if mod(j,2) == 0
+            R = [i+1, j;
+                 i+1, j-1;
+                 i,   j-1;
+                 i-1, j];
+        else
+            R = [i+1, j;
+                 i,   j-1;
+                 i-1, j-1;
+                 i-1, j];
+        end
+    end
 end
-
-% Exclude corner cases
-R(any(R == 0,2),:) = [];
-R(R(:,1) == M+1,:) = [];
-R(R(:,2) == N+1,:) = [];
 
 % Convert to column major indexing
 R =  R(:,1) + (R(:,2) - 1) * M;
--- main.m
+++ main.m
@@ -5,21 +5,25 @@ close all
 % Type of game: 'Simple', 'Punishment', 'Reputation'
 mode = 'Simple';
 if strcmp(mode, 'Simple')
-    L = unidrnd(2,M,N) - 1;
+    % Minimum and maximum strategy numbers (See strategy matrix below)
+    minStr = 0;
+    maxStr = 1;
     % meet function handle
     meet = @meetSimple;
     
 elseif strcmp(mode, 'Punishment')
-    L = randi([0 3], M, N);
+    % Minimum and maximum strategy numbers (See strategy matrix below)
+    minStr = 0;
+    maxStr = 3;
     % meet function handle
     meet = @meetPunishment;
-    fprintf('Initial percentage of cooperators: %0.2f %% \n', ...
-        (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N))
-    printPercentages(L)
     
 elseif strcmp(mode, 'Reputation')
     % meet function handle
     meet = @meetReputation;
+    % Minimum and maximum strategy numbers (See strategy matrix below)
+    minStr = 0;
+    maxStr = 3;
     % TODO
 else
     fprintf("Invalid mode entered.\n")
@@ -49,102 +53,107 @@ beta = 1.5;
 P = zeros(M,N);
 
 for rr = 1:length(rArr)
-% Set rate
-r = rArr(rr);
-
-% Strategy matrix (randomly generated)
-% 0 for defectors
-% 1 for cooperators
-% 2 for defectors with punishment( 'punishment' mode only )
-% 3 for cooperators with punishment( 'punishment' mode only )
-% rng(1)
-% L = unidrnd(2,M,N) - 1;
-L = randi([0 3], M, N);
-fprintf('Initial percentage of cooperators for r=%0.2f: %0.2f %% \n', ...
-    r, (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N))
-printPercentages(L)
-
-% Every 100 rounds check the frequencies of the previous checkpoint. If
-% they are the same then skip the rest rounds to save time
-prev0 = 0;prev1 = 0;prev2 = 0;prev3 = 0;
-% Score matrix initialization
-P = zeros(M,N);
-
-for t = 1:T
-    %% INTERACTION STAGE
+    % Set rate
+    r = rArr(rr);
     
-    % For every point in the lattice
-    for i = 1:M
-        for j = 1:N
-            
-            % Find neighbors
-            R = getNeighbors(i,j,M,N);
-            lR = length(R);
-            
-            % For every neighbor
-            for k = 1:(lR-1)
-                P(i,j) = P(i,j) + meet(L(i,j), L(R(k)), L(R(k+1)), ...
-                    r, c, beta, gamma);
-            end
-            
-            % If there are more than two neighbors,
-            % then meet with the last and first neighbor
-            % and find the mean score
-            if lR > 2
-                P(i,j) = P(i,j) + meet(L(i,j), L(R(k)), L(R(k+1)), ...
-                    r, c, beta, gamma);
-            end
-            P(i,j) = P(i,j) / lR;
-            
-        end
-    end
+    % Strategy matrix (randomly generated)
+    % 0 for defectors
+    % 1 for cooperators
+    % 2 for defectors with punishment( 'punishment' mode only )
+    % 3 for cooperators with punishment( 'punishment' mode only )
+    L = randi([minStr maxStr], M, N);
+    fprintf('Initial percentage of cooperators for r=%0.2f: %0.2f %% \n', ...
+        r, (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N))
+    printPercentages(L)
     
-    %% IMITATION/REPRODUCTION STAGE
+    % Every 100 rounds check the frequencies of the previous checkpoint. If
+    % they are the same then skip the rest rounds to save time
+    prev0 = 0;prev1 = 0;prev2 = 0;prev3 = 0;
+    % Score matrix initialization
+    P = zeros(M,N);
     
-    % For every point in the lattice
-    for i = 1:M
-        for j = 1:N
-            
-            % Find neighbors
-            R = getNeighbors(i,j,M,N);
-            
-            % Difference of the scores compared to the neighbors
-            diff = P(R) - P(i,j);
-            
-            imitationNeighboor = rouletteWheelSelection(diff);
-            if imitationNeighboor ~= -1
-                L(i,j) = L(R(imitationNeighboor));
-            end
-            
-        end
-    end
-    
-    % Convergence condition to avoid uneccessary rounds
-    epsilon = 0.1;
-    if( mod(t, 100) == 0 )
-
-        if( abs(sum(L(:) == 0) - prev0)/(M*N) < epsilon && ...
-            abs(sum(L(:) == 1) - prev1)/(M*N) < epsilon && ...
-            abs(sum(L(:) == 2) - prev2)/(M*N) < epsilon && ...
-            abs(sum(L(:) == 3) - prev3)/(M*N) < epsilon )
-            break
-        end
-        % Update frequencies every 100 rounds
-        prev0 = sum(L(:) == 0);
-        prev1 = sum(L(:) == 1);
-        prev2 = sum(L(:) == 2);
-        prev3 = sum(L(:) == 3);
+    for t = 1:T
+        %% INTERACTION STAGE
         
-        pp = (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N);
-        fprintf('Percentage of cooperators at round %d and for r=%0.2f: %f%% \n', t, r, pp)
-        printPercentages(L);
+        % For every point in the lattice
+        for i = 1:M
+            for j = 1:N
+                
+                % Find neighbors
+                R = getNeighbors(i,j,M,N);
+                lR = length(R);
+                
+                % For every neighbor
+                for k = 1:(lR-1)
+                    P(i,j) = P(i,j) + meet(L(i,j), L(R(k)), L(R(k+1)), ...
+                        r, c, beta, gamma);
+                end
+                
+                % If there are more than two neighbors,
+                % then meet with the last and first neighbor
+                % and find the mean score
+                if lR > 2
+                    P(i,j) = P(i,j) + meet(L(i,j), L(R(k)), L(R(k+1)), ...
+                        r, c, beta, gamma);
+                end
+                P(i,j) = P(i,j) / lR;
+                
+            end
+        end
+        
+        %% IMITATION/REPRODUCTION STAGE
+        
+        % For every point in the lattice
+        for i = 1:M
+            for j = 1:N
+                
+                % Find neighbors
+                R = getNeighbors(i,j,M,N);
+                
+                % Difference of the scores compared to the neighbors
+                diff = P(R) - P(i,j);
+                
+                imitationNeighboor = rouletteWheelSelection(diff);
+                if imitationNeighboor ~= -1
+                    L(i,j) = L(R(imitationNeighboor));
+                end
+                
+            end
+        end
+        
+        % Convergence condition to avoid uneccessary rounds
+        epsilon = 0.1;
+        if( mod(t, 100) == 0 )
+            
+            % Find current frequencies
+            curr0 = sum(L(:) == 0);
+            curr1 = sum(L(:) == 1);
+            curr2 = sum(L(:) == 2);
+            curr3 = sum(L(:) == 3);
+            
+            if( abs(curr0 - prev0)/(M*N) < epsilon && ...
+                    abs(curr1 - prev1)/(M*N) < epsilon && ...
+                    abs(curr2 - prev2)/(M*N) < epsilon && ...
+                    abs(curr3 - prev3)/(M*N) < epsilon )
+                break
+            end
+            
+            % Update frequencies every 100 rounds
+            prev0 = curr0;
+            prev1 = curr1;
+            prev2 = curr2;
+            prev3 = curr3;
+            
+            pp = (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N);
+            fprintf('Percentage of cooperators at round %d and for r=%0.2f: %f%% \n', t, r, pp)
+            printPercentages(L);
+        end
+        
     end
-    
-end
-frequencies(rr, 1) = sum(L(:) == 0);
-frequencies(rr, 2) = sum(L(:) == 1);
-frequencies(rr, 3) = sum(L(:) == 2);
-frequencies(rr, 4) = sum(L(:) == 3);
+    frequencies(rr, 1) = sum(L(:) == 0);
+    frequencies(rr, 2) = sum(L(:) == 1);
+    frequencies(rr, 3) = sum(L(:) == 2);
+    frequencies(rr, 4) = sum(L(:) == 3);
 end
 
 %% Plot results
@@ -156,9 +165,16 @@ figure(2)
 hold on
 plot(rArr, frequencies(:, 1), ':')
 plot(rArr, frequencies(:, 2), '--')
-plot(rArr, frequencies(:, 3), '-.')
-plot(rArr, frequencies(:, 4), '-')
-legend('Antisocial', 'Mild', 'Paradoxical', 'Social')
+legend('Defectors', 'Cooperators')
+
+if strcmp(mode, 'Punishment') || strcmp(mode, 'Reputation')
+    plot(rArr, frequencies(:, 3), '-.')
+    plot(rArr, frequencies(:, 4), '-')
+    legend('Antisocial', 'Mild', 'Paradoxical', 'Social')
+end
+
+xlabel('multiplication factor, r')
+ylabel('frequencies')
 
 fprintf('Final percentage of cooperators: %0.2f %% \n', ...
     (sum(L(:) == 1) + sum(L(:) == 3)) * 100 / (M * N))
